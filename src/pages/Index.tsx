2ba531052764196de1bd36309b76985e6aa471fa
Lzsaam <samuel.lima@sptech.school>
2025-04-28 22:12:02 -0300

    Adicionando pasta

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,13 @@
+import Agenda from "@/components/Agenda";
+import ProfileForm from "@/components/ProfileForm";
 import DisponibilizarHorario from "@/pages/DisponibilizarHorario";
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-[#EDF2FB] py-8">
       {/* <Agenda /> */}
-      <DisponibilizarHorario />
-      {/* <ProfileForm /> */}
+      {/* <DisponibilizarHorario /> */}
+      <ProfileForm />
     </div>
   );
 };
