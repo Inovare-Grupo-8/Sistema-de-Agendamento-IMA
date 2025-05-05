ea0882b3e8e9cc84b5de9818b8278cfa6ca8be3d
Lzsaam <samuel.lima@sptech.school>
2025-05-05 17:13:30 -0300

    Mudando a porta do vite

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 3333,
     historyApiFallback: true, // Redireciona todas as requisições para index.html
   },
   plugins: [
