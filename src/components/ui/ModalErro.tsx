57202846c4444f7e9b0f200e49d2568771c7a779
ryangsantos2 <ryan.santos@sptech.school>
2025-05-21 23:54:40 -0300

    adicionando botao do google e ajustando validações de senha e cpf

--- src/components/ui/ModalErro.tsx
+++ src/components/ui/ModalErro.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface ModalErroProps {
   mensagem: string;
@@ -6,13 +6,22 @@ interface ModalErroProps {
 }
 
 const ModalErro: React.FC<ModalErroProps> = ({ mensagem, onClose }) => {
+  const [contador, setContador] = useState(5);
+
+  useEffect(() => {
+    if (contador === 0) {
+      onClose();
+      return;
+    }
+    const timer = setTimeout(() => setContador(contador - 1), 1000);
+    return () => clearTimeout(timer);
+  }, [contador, onClose]);
+
   return (
     <div className="modal" style={{ display: 'block' }}>
       <div className="modal-conteudo">
         <p>{mensagem}</p>
-        <button className="fechar" onClick={onClose}>
-          Fechar
-        </button>
+        <p className='font-semibold text-sm'>Fechando em <span>{contador}</span> segundos...</p>
       </div>
     </div>
   );
