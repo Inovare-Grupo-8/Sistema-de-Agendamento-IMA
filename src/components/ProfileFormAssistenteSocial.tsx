5aa38d79f4b51a87ecd348112cd5f3df9193b233
jheniferlorrane <jhenifer.rodrigues@sptech.school>
2025-06-08 04:21:43 -0300

    telas da assistente integradas 100%

--- src/components/ProfileFormAssistenteSocial.tsx
+++ src/components/ProfileFormAssistenteSocial.tsx
@@ -86,14 +86,14 @@ export const assistenteSocialNavItems: Record<string, UserNavigationItem> = {
     label: 'Classificar Usuários',
     icon: <UserCheck className="w-6 h-6" color="#ED4231" />,
   },
-  cadastrarVoluntario: {
-    path: '/cadastro-voluntario',
-    label: 'Cadastrar Voluntário',
-    icon: <UserPlus className="w-6 h-6" color="#ED4231" />,
-  },
   cadastrarAssistente: {
     path: '/cadastro-assistente',
     label: 'Cadastrar Assistente', 
+    icon: <UserPlus className="w-6 h-6" color="#ED4231" />,
+  },
+  cadastrarVoluntario: {
+    path: '/cadastro-voluntario',
+    label: 'Cadastrar Voluntário',
     icon: <UserPlus className="w-6 h-6" color="#ED4231" />,
   },
   perfil: {
